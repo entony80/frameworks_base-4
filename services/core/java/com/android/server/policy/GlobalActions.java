fb548f70b91e238f0e36ef9a10a8d5880681b4ff
Chris <chriscrawf95@gmail.com>
2016-06-26 18:13:00 -0400

    Revert "Revert"FWB: Power/reboot dialogs dim amount (1/2) ""
    This reverts commit 0f9bf6e7a95cb6e9009637c21427680b762fa6d7.

--- services/core/java/com/android/server/policy/GlobalActions.java
+++ services/core/java/com/android/server/policy/GlobalActions.java
@@ -354,6 +354,10 @@ class GlobalActions implements DialogInterface.OnDismissListener, DialogInterfac
             }
 
             mDialog.getWindow().setAttributes(attrs);
+            if (Settings.System.getInt(mContext.getContentResolver(),
+                    Settings.System.TRANSPARENT_POWER_DIALOG_DIM, 50) != 50) {
+                mDialog.getWindow().setDimAmount(setPowerMenuDialogDim());
+            }
             mDialog.show();
             mDialog.getWindow().getDecorView().setSystemUiVisibility(View.STATUS_BAR_DISABLE_EXPAND);
         }
@@ -370,6 +374,14 @@ class GlobalActions implements DialogInterface.OnDismissListener, DialogInterfac
         double dAlpha = mPowerMenuAlpha / 100.0;
         float alpha = (float) dAlpha;
         return alpha;
+    }
+
+    private float setPowerMenuDialogDim() {
+        int mPowerMenuDialogDim = Settings.System.getInt(mContext.getContentResolver(),
+                Settings.System.TRANSPARENT_POWER_DIALOG_DIM, 50);
+        double dDim = mPowerMenuDialogDim / 100.0;
+        float dim = (float) dDim;
+        return dim;
     }
 
     private Context getUiContext() {
