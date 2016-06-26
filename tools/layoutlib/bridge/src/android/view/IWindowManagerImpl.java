d0d8268aeeed672f9449522f428f3b574ea73d2b
Chris <chriscrawf95@gmail.com>
2016-06-26 18:11:58 -0400

    Revert "fix derp"
    This reverts commit e93a12179c39d563fe1d0075c775cc7024665ab4.

--- tools/layoutlib/bridge/src/android/view/IWindowManagerImpl.java
+++ tools/layoutlib/bridge/src/android/view/IWindowManagerImpl.java
@@ -511,7 +511,13 @@ public class IWindowManagerImpl implements IWindowManager {
         // TODO Auto-generated method stub
         return null;
     }
-	
+
+    /* @hide */
+    @Override
+    public int getSystemUIVisibility() {
+        return 0;
+    }
+
     /* @hide */
     @Override
     public void toggleGlobalMenu() {
