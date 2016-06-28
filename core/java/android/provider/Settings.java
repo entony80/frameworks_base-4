d9e55d4442b519924f60cf031f66ddece06158d0
Chris <chriscrawf95@gmail.com>
2016-06-27 20:46:01 -0400

    Revert"AOKP custom system animations (1/2)"
    This reverts commit 506fdac

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4863,24 +4863,6 @@ public final class Settings {
          */
         public static final String DASHBOARD_TILEVIEW_DOUBLE_LINES = "dashboard_tileview_double_lines";
 
-        /**  
-         * AOKP Custom System Animations
-         * @hide
-         */  
-        public static final String[] ACTIVITY_ANIMATION_CONTROLS = new String[] {  
-                "activity_open",
-                "activity_close",
-                "task_open",  
-                "task_close",  
-                "task_to_front",
-                "task_to_back",  
-                "wallpaper_open",  
-                "wallpaper_close",  
-                "wallpaper_intra_open",  
-                "wallpaper_intra_close",  
-        };  
-        public static final String ANIMATION_CONTROLS_DURATION = "animation_controls_duration";
-
         /**
          * Change the number of columns displayed in the Settings dashboard
          * @hide
