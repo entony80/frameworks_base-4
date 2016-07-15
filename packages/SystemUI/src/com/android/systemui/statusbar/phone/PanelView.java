73560d118f83464e336334162f0f80aa6e70a9b2
Chris <chriscrawf95@gmail.com>
2016-07-14 21:49:17 -0400

    Revert "FWB: Revert to old clear all implementation"
    This reverts commit 650ef114ba8ed70b311125035756a7098d3447bc.

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PanelView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PanelView.java
@@ -704,35 +704,19 @@ public abstract class PanelView extends FrameLayout {
 
             @Override
             public void onAnimationEnd(Animator animation) {
+                if (expand) PanelView.this.removeOnLayoutChangeListener(mLayoutChangeListener);
                 if (clearAllExpandHack && !mCancelled) {
-                    mHeightAnimator = createHeightAnimator(getMaxPanelHeight());
-                    mHeightAnimator.setInterpolator(mLinearOutSlowInInterpolator);
-                    mHeightAnimator.setDuration(350);
-                    mHeightAnimator.addListener(new AnimatorListenerAdapter() {
-                        private boolean mCancelled;
-
-                        @Override
-                        public void onAnimationCancel(Animator animation) {
-                            mCancelled = true;
-                        }
-
-                        @Override
-                        public void onAnimationEnd(Animator animation) {
-                            mHeightAnimator = null;
-                            if (!mCancelled) {
-                                notifyExpandingFinished();
-                            }
-                        }
-                    });
-                    mHeightAnimator.start();
-                } else {
-                    mHeightAnimator = null;
-                    if (!mCancelled) {
-                        notifyExpandingFinished();
-                    }
-				}
+                    setExpandedHeightInternal(getMaxPanelHeight());
+                }
+                mHeightAnimator = null;
+                if (!mCancelled) {
+                    notifyExpandingFinished();
+                }
+                notifyBarPanelExpansionChanged();
             }
         });
+        mHeightAnimator = animator;
+        animator.start();
     }
 
     @Override
