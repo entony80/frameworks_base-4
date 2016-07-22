7de60888ba27ddfd0231214294625d63827d3213
d34d <clark@cyngn.com>
2016-07-22 13:24:29 -0700

    SysUI: Show hints if showing keyguard and provisioned
    Change-Id: Ifd8923e7eabc7b8fd9186dbf757272dec3a1a8b1
    TICKET: NIGHTLIES-3199 NIGHTLIES-3202 NIGHTLIES-3204

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/NotificationPanelView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/NotificationPanelView.java
@@ -2710,9 +2710,11 @@ public class NotificationPanelView extends PanelView implements
 
     public void onScreenTurningOn() {
         mKeyguardStatusView.refreshTime();
-        startScreenOnHintAnimation(mLiveLockscreenController.isLiveLockScreenInteractive() &&
-                !mUserInteractedWithLiveLockScreen,
-                !mUserUnlocked, !mUserExpandedNotifications);
+        if (shouldShowScreenOnHints()) {
+            startScreenOnHintAnimation(mLiveLockscreenController.isLiveLockScreenInteractive() &&
+                            !mUserInteractedWithLiveLockScreen,
+                    !mUserUnlocked, !mUserExpandedNotifications);
+        }
     }
 
     @Override
@@ -3158,5 +3160,9 @@ public class NotificationPanelView extends PanelView implements
 
     private boolean getUserReturnedFromLls() {
         return getSharedPreferenceBoolean(KEY_USER_RETURNED_FROM_LLS, false);
+    }
+
+    private boolean shouldShowScreenOnHints() {
+        return mStatusBar.isDeviceProvisioned() && mStatusBarState == StatusBarState.KEYGUARD;
     }
 }
